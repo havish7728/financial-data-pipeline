49979535d619fc5c531f13c5400b3f2d8951fc55
Havish7728 <havish7728@gmail.com>
2025-04-03 19:13:05 +0530

    Configurations updated

---
+++ config.py
@@ -0,0 +1,12 @@
+DB_CONFIG={
+    'user': 'root',
+    'password': 'root',
+    'host': 'localhost',
+    'database': 'financial_db'
+}
+
+AWS_CONFIG = {
+    'aws_access_key_id': 'AKIATFBMPFP67DTJLCOT',
+    'aws_secret_access_key': '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',
+    'bucket_name': 'financial-data-bucket-1',
+}
